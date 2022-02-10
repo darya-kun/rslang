c9c4ff30ad0599d57226561cb49b223e666a1efd
kolyamoloko <kolyamoloko@mail.ru>
2022-02-10 21:21:22 +0300

    add: 5 async function

--- rslang/src/components/Api/Api.js
+++ rslang/src/components/Api/Api.js
@@ -1,12 +1,66 @@
-const serverName = 'https://react-learnwords-example.herokuapp.com/';
+export default class Service {
+  _apiBase = 'https://react-learnwords-example.herokuapp.com/';
 
-export const getChunkOfWords = async (group, page) => {
-  const response = await fetch(`${serverName}words?group=${group}&page=${page}`,{
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  const data = await response.json();
-  return data;
-}
+  async getChunkWords (group, page){
+    const response = await fetch(`${this._apiBase}words?group=${group}&page=${page}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return await response.json();
+  };
+
+  async getWords (id){
+    const response = await fetch(`${this._apiBase}words/${id}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return await response.json();
+  };
+
+  async createUser(user) {
+    const response = await fetch(this._apiBase + 'users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    });
+    return await response.json();
+  };
+
+  async getUser (id){
+    const response = await fetch(`${this._apiBase}users/${id}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return await response.json();
+  };
+
+  async updateUser (id){
+
+    const user = {
+      name,
+      email,
+      password,
+    };
+    
+    const response = await fetch(`${this._apiBase}users/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    });
+  };
+};
