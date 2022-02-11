71ac126f21744128eb02534a54fe2256a769bc6f
Darya <darya.drazdouskaya@gmail.com>
2022-02-11 14:47:56 +0300

    feat: add base structure for audiocall game

---
+++ rslang/src/components/AudiocallGame/AudiocallGame.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AudiocallGame.css'
+
+function AudiocallGame() {
+  return (
+    <main className='main'>
+      <div>Audiocall Game page</div>
+    </main>
+  )
+}
+
+export default AudiocallGame;
