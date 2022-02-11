d44bc3aa62590537694aaa83458f2babd6c18ddb
Darya <darya.drazdouskaya@gmail.com>
2022-02-11 14:47:01 +0300

    feat: add game cards

--- rslang/src/components/Games/Games.js
+++ rslang/src/components/Games/Games.js
@@ -1,10 +1,49 @@
 import React from 'react';
-
+// import PageListItem from '../PageListItem/PageListItem';
+import { Link } from 'react-router-dom';
+// import SprintGame from '../SprintGame/SpringGame';
+// import AudiocallGame from '../AudiocallGame/AudiocallGame';
+// import { Route, Routes } from 'react-router-dom';
+import './games.css';
 
 const Games = () => {
-  return   (
+  // const gameCard = [
+  //   { id: 1, selected: false, title: 'Спринт', src: './img/carrot.jpg', link: '/sprint'},
+  //   { id: 2, selected: false, title: 'Аудиовызов', src: './img/okra.jpg', link: '/audiocall'},
+  // ]
+
+  // const elements = cards.map((item) => {
+  //   return <li><Link to={item.link}><PageListItem title={item.title} src={item.src}/></Link></li>
+  // });
+
+  return (
     <main className='main'>
-      <div>Games Page</div>
+      <div className="cards-container">
+        <Link to='/sprint'>
+          <div className='card card_game'>
+            <img src='' className='card-image' alt='sprint card'></img>
+            <div className='card-contain'>
+              <div className='card-contain__text'>
+                <span className='card-contain__text_title'>Спринт</span>
+              </div>
+            </div>
+          </div>
+        </Link>
+        <Link to='/audiocall'>
+          <div className='card card_game'>
+            <img src='' className='card-image' alt='sprint card'></img>
+            <div className='card-contain'>
+              <div className='card-contain__text'>
+                <span className='card-contain__text_title'>Аудиовызов</span>
+              </div>
+            </div>
+          </div>
+        </Link>
+      </div>
+      {/* <Routes>
+        <Route path='/sprint' element={<SprintGame/>} />
+        <Route path='/audiocall' element={<AudiocallGame/>} /> 
+      </Routes> */}
     </main>
   )
 }
