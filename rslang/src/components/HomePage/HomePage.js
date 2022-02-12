00d8c6bd2563eb794e2fe9f488ef5e01a8a26513
AlionaMu <udova2000@mail.ru>
2022-02-12 23:01:50 +0300

    feat: add about-us section on home-page

--- rslang/src/components/HomePage/HomePage.js
+++ rslang/src/components/HomePage/HomePage.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import PageListItem from '../PageListItem/PageListItem';
+import AboutUsCard from '../AboutUsCard/AboutUsCard';
 import { Link } from 'react-router-dom';
+import './homePage.css';
 
 function HomePage({cards}) {
+  const aboutUs = [
+    { id: 1, title: 'darya-kun', src: '/img/cat-books.png'},
+    { id: 2, title: 'alionamu', src: '/img/cat-pencil.png'},
+    { id: 3, title: 'kolyamoloko', src: '/img/cat-backpack.png'}
+   ]
+
   const elements = cards.map((item) => {
     return <div key={item.id}><Link to={item.link}><PageListItem title={item.title} src={item.src}/></Link></div>
+  });
+
+  const aboutUsCards = aboutUs.map((item) => {
+    return <div key={item.id}><AboutUsCard title={item.title} src={item.src}/></div>
   });
   return (
     <main className='main'>
       <div className="cards-container">
         {elements}
+      </div>
+      <div class="about-us">
+        <h3 class="about-us__title">About us</h3>
+          <div class="about-us__content">
+            {aboutUsCards}        
+          </div>     
       </div>
     </main>
   )
