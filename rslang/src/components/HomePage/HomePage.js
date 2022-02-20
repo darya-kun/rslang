e9112af347961c1d4f4a48c8d7af6a474489c2e2
Darya <darya.drazdouskaya@gmail.com>
2022-02-20 21:02:36 +0300

    feat: add github's link and description

--- rslang/src/components/HomePage/HomePage.js
+++ rslang/src/components/HomePage/HomePage.js
@@ -7,9 +7,9 @@ import './homePage.css';
 
 function HomePage({cards}) {
   const aboutUs = [
-    { id: 1, title: 'darya-kun', src: '/img/avatar-darya.png'},
-    { id: 2, title: 'alionamu', src: '/img/avatar-aliona.png'},
-    { id: 3, title: 'kolyamoloko', src: '/img/avatar-kolya.png'}
+    { id: 1, title: 'Даша', src: '/img/avatar-darya.png', githubLink: 'https://github.com/darya-kun', description: 'Ответственная за дизайн приложения, его верстку и котиков :) Разработала главную страницу, пагинацию в учебнике, начальные экраны игр, а также переключатель light и dark темы.' },
+    { id: 2, title: 'Алёна', src: '/img/avatar-aliona.png', githubLink: 'https://github.com/AlionaMu', description: 'Тимлид, ответственная за архитектуру приложения. Разработала Учебник и игру "Аудиовызов".' },
+    { id: 3, title: 'Коля', src: '/img/avatar-kolya.png', githubLink: 'https://github.com/kolyamoloko', description: 'Ответственный за бэкенд, регистрацию и авторизацию. Разработал игру "Спринт", а также привел в порядок пагинацию в учебнике.' }
    ]
 
   const elements = cards.map((item) => {
@@ -17,7 +17,7 @@ function HomePage({cards}) {
   });
 
   const aboutUsCards = aboutUs.map((item) => {
-    return <div key={item.id}><AboutUsCard title={item.title} src={item.src}/></div>
+    return <div key={item.id}><AboutUsCard title={item.title} src={item.src} githubLink={item.githubLink} description={item.description}/></div>
   });
 
   return (
