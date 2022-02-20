044cae6b4e3a9bc651a38ff597c1d1c223152b6c
Darya <darya.drazdouskaya@gmail.com>
2022-02-20 20:58:29 +0300

    feat: change github's link and user name

--- rslang/src/components/Footer/Footer.js
+++ rslang/src/components/Footer/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
     <footer className='footer'>
       <div className="container">
         <div className="footer__wrapper">
-          <a className="github" href="https://github.com">GitHub-1</a>
-          <a className="github" href="https://github.com">GitHub-2</a>
-          <a className="github" href="https://github.com">GitHub-3</a>
+          <a className="github" href="https://github.com/darya-kun">darya-kun</a>
+          <a className="github" href="https://github.com/AlionaMu">AlionaMu</a>
+          <a className="github" href="https://github.com/kolyamoloko">kolyamoloko</a>
           <a className="rss" href="https://rs.school/js/">
             <span className="rss-year">'2022</span>
           </a>
